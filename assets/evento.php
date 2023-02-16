97ce2ae3966d11d14db6940f4d9ef75305d35725
Zorz4 <118759989+Z0rz4@users.noreply.github.com>
2023-02-16 11:28:58 -0300

    Add files via upload

---
+++ assets/evento.php
@@ -0,0 +1,11 @@
+<?php
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $telefone = $_POST["telefone"];
+    echo "nome:".$name;
+    echo "<br>";
+    echo "email:".$email;
+    echo "<br>";
+    echo "telefone:".$telefone;
+    echo "<br>";
+?>
